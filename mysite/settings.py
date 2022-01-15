9836060f8d9f42a348182b9575649d1ee2243646
Bourbonsz <93754391+Bourbonsz@users.noreply.github.com>
2022-01-15 15:44:51 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrb+avu_zcx)hzaabha(i81#lm9r%mvv-polvhpw)z5&c4=ws2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
